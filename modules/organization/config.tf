0998dc08d0885862ab1626edf16ad51677e17020
J.R. Mash <2574997+jrmash@users.noreply.github.com>
2024-10-15 09:51:58 -0700

    Bump version constraint for `organization` module

--- modules/organization/config.tf
+++ modules/organization/config.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.20"
+      version = ">= 5.20, <= 6.0"
     }
   }
   required_version = "~> 1.3"
